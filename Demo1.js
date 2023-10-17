39bf2d279e6d2bf8fa0bc554e768f3c6c057c282
mattchristenson <matt@rcc.team>
2023-10-17 10:31:47 -0500

    first part of lecture.

---
+++ Demo1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var firstName;
+
+firstName = "Matt";
+
+// firstName = 'Matt';
+
+// var bookTitle;
+// bookTitle = "Matt's Advice on life";
+// bookTitle = 'Matt is writing his book "Advice on life"';
+// bookTitle = "Matt wrote the book \"Matt's advice on life\"";
+
+
+console.log(firstName);
+
+
+
